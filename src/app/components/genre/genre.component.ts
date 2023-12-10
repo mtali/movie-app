0c5671bcc90b41619d9f35815e1f88f16d7bafb9
mtali <emmanuelmtali@yahoo.com>
2023-12-10 04:25:43 +0300

    Show movie listings by genre

--- src/app/components/genre/genre.component.ts
+++ src/app/components/genre/genre.component.ts
@@ -1,9 +1,38 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {MoviesService} from "../../service/movies.service";
+import {ActivatedRoute, Params} from "@angular/router";
 
 @Component({
   selector: 'app-genre',
   templateUrl: './genre.component.html'
 })
-export class GenreComponent {
+export class GenreComponent implements OnInit {
 
+  id!: number;
+  title!: string;
+  loading: boolean = true;
+  moviesGenre: any;
+
+  constructor(
+    private moviesService: MoviesService,
+    private router: ActivatedRoute
+  ) {
+  }
+
+  ngOnInit() {
+    this.router.params.subscribe((params: Params) => {
+      this.id = params["id"];
+      this.title = params["name"];
+      console.log(this.id, this.title)
+      this.getMoviesByGenre(this.id)
+    });
+  }
+
+  getMoviesByGenre(id: number) {
+    this.moviesService.getMoviesByGenre(id).subscribe((res: any) => {
+      console.log(res)
+      this.moviesGenre = res.results;
+      this.loading = false;
+    });
+  }
 }
